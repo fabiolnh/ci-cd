aa1370a923d7b54d87e1e69ce09c12923ce0dd6c
Fabio Lamartine Nogueira Henriques <fabiolnh@gmail.com>
2022-03-21 20:55:22 -0300

    refactor: change method names

--- app/math.go
+++ app/math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(soma2(112, 21))
+	fmt.Println(soma(112, 21))
 }
 
-func soma2(a int, b int) int {
+func soma(a int, b int) int {
 	return a + b
 }
--- app/math_test.go
+++ app/math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := soma2(16, 16)
+	total := soma(16, 16)
 
 	if total != 32 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 32)
